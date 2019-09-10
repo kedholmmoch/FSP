cf28f8965c5c68db01123cccbc6b8ef1e30f244f
kmoch <kevin.edholm.moch@gmail.com>
2019-09-09 17:01:29 -0700

    working session_api_util ajax requests

--- frontend/materiaApp.jsx
+++ frontend/materiaApp.jsx
@@ -2,7 +2,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//testing
+
+import {
+    postUser,
+    postSession,
+    deleteSession
+} from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
+    
+    // testing start
+
+    window.postUser = postUser;
+    window.postSession = postSession;
+    window.deleteSession = deleteSession;
+
+    // testing end
+    
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Materia</h1>, root);
 });
