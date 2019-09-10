07aed8c6f4061a10436fc3f9cf5824c20ba2f41f
kmoch <kevin.edholm.moch@gmail.com>
2019-09-09 23:09:05 -0700

    state reducers and page displays app via root and entry

--- frontend/materiaApp.jsx
+++ frontend/materiaApp.jsx
@@ -1,25 +1,32 @@
 // React
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
 
 //testing
 
-import {
-    postUser,
-    postSession,
-    deleteSession
-} from './util/session_api_util';
+// import {
+//     postUser,
+//     postSession,
+//     deleteSession
+// } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
     
+    const store = configureStore();
+
     // testing start
 
-    window.postUser = postUser;
-    window.postSession = postSession;
-    window.deleteSession = deleteSession;
+    // window.postUser = postUser;
+    // window.postSession = postSession;
+    // window.deleteSession = deleteSession;
+
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
 
     // testing end
     
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>Materia</h1>, root);
+    ReactDOM.render(<Root store={store}/>, root);
 });
---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,19 @@
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+
+const usersReducers = (state = {}, action) => {
+    Object.freeze(state);
+    let newState;
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            newState = Object.assign(
+                {}, 
+                state, 
+                {[action.currentUser.id]: action.currentUser}
+            );
+            return newState;
+        default:
+            return state;
+    }
+};
+
+export default usersReducers;
