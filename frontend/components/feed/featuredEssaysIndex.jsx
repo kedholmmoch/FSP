c1886caaa1e7e690aef416d2ecd50c5903e75fd3
kmoch <kevin.edholm.moch@gmail.com>
2019-09-13 18:09:56 -0700

    finally got items to display after fetchEssays call in index component

--- frontend/components/feed/featuredEssaysIndex.jsx
+++ frontend/components/feed/featuredEssaysIndex.jsx
@@ -1,31 +1,83 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 class FeaturedEssaysIndex extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            isFetching: true,
+        };
+    }
+
+    componentDidMount() {
+        
+        this.props.fetchEssays({filter: "featured"})
+            .then(() => this.setState({ isFetching: false }))
     }
 
     render() {
+
+        const { isFetching } = this.state;
+
+         // let featured = this.props.essays;
+
+        // if (featured === undefined) {
+        //     this.setState({ isFetching: true })
+        // }
+
+        // if (!this.state.isFetching) {
+
+        console.log(moment().format());
         
-        
+        let featured = this.props.essays;
+        // console.log(featured);
+
+        // let featuredOne = featured[0];
+        // console.log(featuredOne);
+            
+        let featureOne = featured[0];
+        // console.log(featureOne);
+        let featureTwo = featured[1];
+        let featureList = featured.slice(2, 5);
+
+        function mainFeatureDisplay(essay) {
+            const datetime = moment(essay["published_at"]).format("MMM Do");
+
+            return(
+            <div>
+                <Link to={`/essays/${essay.id}`}><img src={essay.image_url} alt={essay.title} /></Link>
+                <Link to={`/essays/${essay.id}`}>{essay.title}</Link>
+                <Link to={`/essays/${essay.id}`}>{essay.summary}</Link>
+                <Link to={`/users/${essay.user_id}`}></Link>
+                <h4>{datetime} &nbsp;&nbsp;&nbsp;&nbsp; 5 min read </h4>
+            </div>
+            );
+        };
+
         const linksMiniList = (
             <ul className="featured-mini-list">
                 <li>Mini</li>
                 <li>List</li>
             </ul>
         );
-        
-        return(
-            <div className="featured-essays-container">
-                <ul className="featured-essays-list-wide">
-                    <article className="first-feature">First Feature</article>
-                    {linksMiniList}
-                    <article className="second-feature">Second Feature</article>
-                </ul>
-                <h6>Featured Essays Index</h6>
-            </div>     
-        )
+            
+            return(
+                <div>
+                { isFetching ? <div></div> : (
+                <div className="featured-essays-container">
+                    <ul className="featured-essays-list-wide">
+                        <article className="first-feature">{mainFeatureDisplay(featureOne)}</article>
+                        {linksMiniList}
+                        <article className="second-feature">{mainFeatureDisplay(featureTwo)}</article>
+                    </ul>
+                    <h6>Featured Essays Index</h6>
+                </div>
+                )}
+                </div>     
+            )
+        }
     };
-}
 
 export default FeaturedEssaysIndex;
--- frontend/components/feed/featuredEssaysIndexContainer.js
+++ frontend/components/feed/featuredEssaysIndexContainer.js
@@ -1,17 +1,21 @@
 import { connect } from 'react-redux';
+
 import FeaturedEssaysIndex from './featuredEssaysIndex';
+import { fetchEssays } from '../../actions/essay_actions';
 
 const mapStateToProps = state => {
 
+    const essays = Object.values(state.entities.essays)
+    
     return {
-
+        essays: essays,
     };
 };
 
 const mapDispatchToProps = dispatch => {
 
     return {
-
+        fetchEssays: filters => dispatch(fetchEssays(filters))
     };
 };
 
